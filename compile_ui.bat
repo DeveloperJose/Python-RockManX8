2905f7b22cb11855f8fabea61add4035011ba652
DeveloperJose <josegperez@mail.com>
2019-07-04 15:37:46 -0600

    First version of editor, mostly the UI

---
+++ compile_ui.bat
@@ -0,0 +1,2 @@
+activate py36
+pyuic5 editor_v2.ui -o editor_v2.py
--- gui.py
+++ gui.py
@@ -1,20 +1,24 @@
-import PyQt5
-from dialog_editor import Ui_MainWindow  # importing our generated file
-from mcb_editor import Ui_MCBWidget
+import os
 import sys
 
-class mywindow(PyQt5.QtWidgets.QMainWindow):
-    def action_open(self):
-        print("yay")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QInputDialog
 
-    def __init__(self):
-        super(mywindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+from ui_editor_window import EditorWindow
 
-        self.ui.actionOpen_X8_Text_File.triggered.connect(self.action_open)
+LANGUAGES = ['SPA - [Spanish]', 'ENG - [English]', 'CHI - [Chinese]', 'FRE - [French]', 'GER - [German]', 'ITA - [Italian]', 'JPN - [Japanese]', 'KOR - [Korean]', 'USA - [PS2 English]']
 
-app = PyQt5.QtWidgets.QApplication([])
-application = mywindow()
-application.show()
-sys.exit(app.exec())
+app = QtWidgets.QApplication([])
+
+# == Settings
+prompt, language_selected = QInputDialog.getItem(None, 'Language Select', 'Please select the language to edit', LANGUAGES, 0, False)
+if language_selected:
+    language = prompt.split('-')[0].strip()
+
+    installation_path = ''
+    while not os.path.exists(os.path.join(installation_path, 'mes', language)):
+        installation_path = str(QFileDialog.getExistingDirectory(None, caption='Please select your X8 installation folder (must contain a /mes/ directory)', directory=''))
+
+    application = EditorWindow(installation_path, language)
+    application.show()
+    sys.exit(app.exec())
