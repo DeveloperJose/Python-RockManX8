424a01ba0d51a80659878460f175352af229345c
Rainfall Pianist <rainfallpianist@gmail.com>
2019-07-05 05:21:53 -0600

    Both Sentry and UI exception handlers work good

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,18 +1,25 @@
 import os
 import sys
+from typing import ClassVar
 
 from PyQt5.QtWidgets import QFileDialog, QInputDialog, QApplication
 from fbs_runtime.application_context import cached_property, is_frozen
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from fbs_runtime.excepthook import ExceptionHandler
 from fbs_runtime.excepthook.sentry import SentryExceptionHandler
 
 from ui_editor_window import EditorWindow
+from ui_design import Ui_MainWindow
 
 
 class AppContext(ApplicationContext):
+    ui: Ui_MainWindow = None
+
     @cached_property
     def exception_handlers(self):
         result = super().exception_handlers
+        result.append(ExHandler(self))
+
         if is_frozen():
             result.append(self.sentry_exception_handler)
         return result
@@ -22,8 +29,19 @@ class AppContext(ApplicationContext):
         return SentryExceptionHandler(
             self.build_settings['sentry_dsn'],
             self.build_settings['version'],
-            self.build_settings['environment']
-        )
+            self.build_settings['environment'])
+
+
+class ExHandler(ExceptionHandler):
+    app_context: AppContext
+
+    def __init__(self, app_context):
+        self.app_context = app_context
+
+    def handle(self, exc_type, exc_value, enriched_tb):
+        if self.app_context.ui is not None:
+            msg = "{}".format(exc_value)
+            self.app_context.ui.statusbar.showMessage(msg, 30000)
 
 
 LANGUAGES = ['SPA - [Spanish]', 'ENG - [English]', 'CHI - [Chinese]', 'FRE - [French]', 'GER - [German]', 'ITA - [Italian]', 'JPN - [Japanese]', 'KOR - [Korean]', 'USA - [PS2 English]']
@@ -54,6 +72,8 @@ if __name__ == '__main__':
         sys.exit(0)
 
     application = EditorWindow(installation_path, language)
+    appctxt.ui = application.ui
+
     application.show()
     exit_code = appctxt.app.exec_()
     sys.exit(exit_code)
