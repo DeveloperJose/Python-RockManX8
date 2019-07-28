8ceb0eb34204d5e0dc6e558486fdbf8bd6d18379
Rainfall Pianist <rainfallpianist@gmail.com>
2019-07-28 03:39:52 -0600

    Exception handling using sentry, console, and gui

--- src/app/app_manager.py
+++ src/app/app_manager.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication
 
 from gui.windows import EditorWindow
-from app import config, mcb_manager
+from app import config, mcb_manager, exception_manager
 
 
 class AppManager:
@@ -12,12 +12,10 @@ class AppManager:
         return config.load_config_or_default()
 
     def __init_editor__(self):
+        mcb_manager.extract_collection_arcs()
         self.editor_window = EditorWindow()
         self.editor_window.setWindowTitle(config.window_title)
         self.editor_window.show()
-
-    def __init_sentry__(self):
-        pass
 
     def log_ui(self, message, *format_args):
         if self.editor_window is None:
@@ -30,15 +28,14 @@ class AppManager:
         if not self.__init_config__():
             return 0
 
-        mcb_manager.extract_collection_arcs()
-
         self.__init_editor__()
-        self.__init_sentry__()
 
         if config.is_valid_collection:
             self.log_ui('Editing X8 from the X Legacy Collection 2')
         else:
             self.log_ui('Editing X8 from PC version released in 2004')
 
+        exception_manager.in_qt_loop = True
+        exception_manager.window = self.editor_window
         exit_code = self.app.exec_()
         return exit_code
