1e36c666739e7209f0c7eedc81b310cca9d2d570
DeveloperJose <josegperez@mail.com>
2019-07-28 03:39:52 -0600

    Exception handling using sentry, console, and gui

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ If you have a 64-bit Windows machine, you can download the pre-bundled package f
 * PyQt5 >= 5.13
 * numpy >= 1.17
 * Pillow >= 6.1
-* PyInstaller >= 3.5
 * sentry-sdk >= 0.10.2
 * qimage2ndarray >= 1.8
+* PyQt5-Tools >= 5.11.3 (if you update the QT Designer .ui files)
+* PyInstaller >= 3.5 (if you plan on bundling the package into one executable)
 
 ## Running (from source)
 Clone this repository using git, then run *"gui/main.py"*
---
+++ src/app/exception_manager.py
@@ -0,0 +1,38 @@
+import traceback
+import sentry_sdk
+import sys
+from PyQt5.QtWidgets import QMessageBox, QApplication
+
+in_qt_loop = False
+window = None
+
+
+def handle_exception(type, value, tb):
+    # Send exception to my Sentry dashboard (only on live release, not debug)
+    if getattr(sys, 'frozen', True):
+        sentry_sdk.capture_exception(value)
+
+    # Print exception traceback on console
+    traceback.print_tb(tb)
+
+    # Display exception in a GUI message box
+    mbox = QMessageBox(window)
+    mbox.setModal(True)
+    mbox.setIcon(QMessageBox.Warning)
+    mbox.setWindowTitle('An error has occurred! [uh-oh?]')
+
+    if isinstance(value, PermissionError):
+        msg = 'Permission denied while trying to write to file: {}'.format(value.filename)
+        info = 'That file is most likely read-only. Please make sure to disable that on the file/folder properties and trying again!'
+    else:
+        msg = 'Error: {}'.format(value)
+        info = 'An unknown error has occured. RainfallPianist has been sent the error information and may be able to provide assistance.'
+
+    mbox.setText(msg)
+    mbox.setInformativeText(info)
+    mbox.setDetailedText(str(traceback.format_tb(tb)))
+    mbox.show()
+
+    if not in_qt_loop:
+        app = QApplication([])
+        sys.exit(app.exec_())
