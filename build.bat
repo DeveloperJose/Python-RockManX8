b6925311f697182a1a74a2bf3c4987613cd5fc73
DeveloperJose <josegperez@mail.com>
2019-11-22 20:46:42 -0700

    Updated PyInstaller build script

--- build.bat
+++ build.bat
@@ -1,14 +1,8 @@
 @ECHO ON
 CALL activate pyqt5
 
-echo y | pyinstaller src/main.py ^
-        --add-data resources/font.wpg;resources/font.wpg ^
-        --add-data resources/mugshots.npz;resources/mugshots.npz ^
-        --add-data resources/ARCtool.exe;resources/ARCtool.exe ^
-        --hidden-import "sentry_sdk.integrations.logging" ^
-        --hidden-import "sentry_sdk.integrations.stdlib" ^
-        --hidden-import "sentry_sdk.integrations.excepthook" ^ 
-        --hidden-import "sentry_sdk.integrations.dedupe"
-        REM --log-level=DEBUG ^
-        REM --upx-dir tools/upx-3.95-win64        
+echo y | pyinstaller src/main_mcb_editor.py ^
+        --add-data resources/default_font.wpg;resources ^
+        --add-data resources/mugshots.pkl;resources ^
+        --add-data resources/ARCtool.exe;resources
 @ECHO ON
